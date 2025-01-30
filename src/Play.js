3d29c82ae48773911910ac479f1748be59307240
Garrett Jeltema <gcjeltema@gmail.com>
2025-01-29 22:52:29 -0800

    first commit

---
+++ src/Play.js
@@ -0,0 +1,108 @@
+class Play extends Phaser.Scene {
+    constructor() {
+        super('playScene')
+    }
+
+    init() {
+        // useful variables
+        this.SHOT_VELOCITY_X_MIN = 200
+        this.SHOT_VELOCITY_X_MAX = 600
+        this.SHOT_VELOCITY_Y_MIN = 700
+        this.SHOT_VELOCITY_Y_MAX = 1100
+        this.ballDestroyed = false
+        this.oneWayCollide = false
+    }
+
+    preload() {
+        this.load.path = './assets/img/'
+        this.load.image('grass', 'grass.jpg')
+        this.load.image('cup', 'cup.jpg')
+        this.load.image('ball', 'ball.png')
+        this.load.image('wall', 'wall.png')
+        this.load.image('oneway', 'one_way_wall.png')
+    }
+
+    create() {
+        // add background grass
+        this.grass = this.add.image(0, 0, 'grass').setOrigin(0)
+
+        this.shotCount = 0
+        this.ballShot = false
+        this.score = 0
+        this.percentageMade = 0
+
+        // add cup
+        this.cup = this.physics.add.sprite(width / 2, height / 10, 'cup')
+        this.cup.body.setCircle(this.cup.width / 4)
+        this.cup.body.setOffset(this.cup.width / 4)
+        this.cup.body.setImmovable(true)
+
+        // add ball
+        this.ball = this.physics.add.sprite(width / 2, height - height / 10, 'ball')
+        this.ball.body.setCircle(this.ball.width / 2)
+        this.ball.body.setCollideWorldBounds(true)
+        this.ball.body.setBounce(0.5)                                   // bounce
+        this.ball.body.setDamping(true).setDrag(0.5)                    // drag
+
+
+        // add walls
+        let wallA = this.physics.add.sprite(0, height / 4, 'wall')
+        wallA.setX(Phaser.Math.Between(0 + wallA.width / 2, width - wallA.width / 2))
+        wallA.body.setImmovable(true)
+
+        let wallB = this.physics.add.sprite(0, height / 2, 'wall')
+        wallB.setX(Phaser.Math.Between(0 + wallB.width / 2, width - wallB.width / 2))
+        wallB.body.setImmovable(true)
+
+        this.walls = this.add.group([wallA, wallB])
+
+        // add one-way
+        this.oneWay = this.physics.add.sprite(0, height / 4 * 3, 'oneway')
+        this.oneWay.setX(Phaser.Math.Between(0 + this.oneWay.width / 2, width - this.oneWay.width / 2))
+        this.oneWay.body.setBounce(1)
+        this.oneWay.body.setImmovable(true)
+        this.oneWay.body.setCollideWorldBounds(true)
+        this.oneWay.body.checkCollision.down = false
+
+        // add one-way moving
+        this.oneWay.body.setVelocityX(100)
+
+        // add pointer input
+        this.input.on('pointerdown', (pointer) => {
+            let shotDirectionX = pointer.x <= this.ball.x ? 1 : -1
+            let shotDirectionY = pointer.y <= this.ball.y ? 1 : -1
+            this.ball.body.setVelocityX(Phaser.Math.Between(this.SHOT_VELOCITY_X_MIN, this.SHOT_VELOCITY_X_MAX) * shotDirectionX)
+            this.ball.body.setVelocityY(Phaser.Math.Between(this.SHOT_VELOCITY_Y_MIN, this.SHOT_VELOCITY_Y_MAX) * shotDirectionY)
+        })
+
+        // cup/ball collision
+        this.physics.add.collider(this.ball, this.cup, (ball, cup) => {
+            this.ballDestroyed = true
+        })
+
+        // ball/wall collision
+        this.physics.add.collider(this.ball, this.walls)
+
+        // ball/one-way collision
+        this.physics.add.collider(this.ball, this.oneWay, (ball, oneWay) => {
+            oneWay.setBounce(0)
+        })
+    }
+
+    update() {
+        // reset ball
+        if(this.ballDestroyed) {
+            this.ball.body.setVelocityX(0)
+            this.ball.body.setVelocityY(0)
+            this.ball.body.reset(width / 2, height - height / 10)
+        }
+    }
+}
+/*
+CODE CHALLENGE
+Try to implement at least 3/4 of the following features during the remainder of class (hint: each takes roughly 15 or fewer lines of code to implement):
+[*] Add ball reset logic on successful shot
+[*] Improve shot logic by making pointer’s relative x-position shoot the ball in correct x-direction
+[*] Make one obstacle move left/right and bounce against screen edges
+[ ] Create and display shot counter, score, and successful shot percentage
+*/
